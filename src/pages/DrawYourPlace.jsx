0f74158ee90a03bc9d6b3d773b683117bf969ac0
= <=>
2018-11-18 02:46:58 +0100

    test

---
+++ src/pages/DrawYourPlace.jsx
@@ -0,0 +1,278 @@
+// @flow
+
+import React, { Fragment } from 'react';
+import type { Meuble } from '../types/Meubles';
+
+type State = {
+    mesMeubles: Meuble[],
+    adaptedValue: Number,
+    selectedMeuble: Number,
+    myPlace: Meuble,
+    newMeuble: Meuble,
+    action: string,
+    isFollowing: boolean,
+    myPlaceIsCreated: boolean,
+    focus: boolean,
+}
+export default class DrawYourPlace extends React.Component<Props> {
+    state = {
+        action: 'CREATE',
+        adaptedValue: 0,
+        mesMeubles: [],
+        selectedMeuble: 0,
+        myPlace: this.props.myPlace,
+        newMeuble: {posX: 0, posY: 0, angle: 0, color: '', name: ''},
+        isFollowing: false,
+        myPlaceIsCreated: false,
+        focus: false,
+    }
+    componentDidMount() {
+        console.log(this.state.myPlace.long , this.myPlaceRef.offsetHeight , this.state.myPlace.larg)
+        if (this.state.myPlace.long >= this.state.myPlace.larg) {
+            console.log('long > larg')
+            console.log(this.state.myPlace.larg, this.myPlaceRef.offsetHeight, this.state.myPlace.long)
+            console.log(this.state.myPlace.larg * this.myPlaceRef.offsetHeight / this.state.myPlace.long)
+            this.setState({adaptedValue: this.state.myPlace.larg * this.myPlaceRef.offsetWidth / this.state.myPlace.long })
+        } else {
+            console.log('larg > long')
+            console.log(this.state.myPlace.long, this.myPlaceRef.offsetHeight, this.state.myPlace.larg)
+            console.log(this.state.myPlace.long * this.myPlaceRef.offsetHeight / this.state.myPlace.larg)
+            this.setState({adaptedValue: this.state.myPlace.long * this.myPlaceRef.offsetHeight / this.state.myPlace.larg })
+        }
+
+        window.addEventListener('keydown',this.handleArrow)
+    }
+
+    addMeuble = () => {
+        this.setState({
+            mesMeubles: [...this.state.mesMeubles, this.state.newMeuble],
+            newMeuble: {
+                posX: 0,
+                posY: 0,
+                larg: 0,
+                long: 0,
+            }
+        });
+    }
+
+    componentWillUpdate = (nextProps, nextState) => {
+        if(this.state.isFollowing !== nextState.isFollowing ) {
+            this.toggleFollowing(nextState.isFollowing);
+        }
+    };
+
+    toggleFollowing = (isEnable) => {
+        if (isEnable) {
+            window.addEventListener('mousemove', this.follow);
+            window.addEventListener('mouseup', this.stopFollow);
+        } else {
+            window.removeEventListener('mousemove', this.follow);
+        }
+    };
+
+    follow = (event) => {
+        this.updateMesMeubles(event.clientX - this.myPlaceRef.offsetLeft - this.state.mesMeubles[this.state.selectedMeuble].long/2, 'posX')
+        this.updateMesMeubles(event.clientY - this.myPlaceRef.offsetTop - this.state.mesMeubles[this.state.selectedMeuble].larg/2, 'posY')
+        console.log(event.clientX - this.myPlaceRef.offsetLeft, event.clientY - this.myPlaceRef.offsetTop)
+    };
+
+    stopFollow =  () => {
+        this.setState({isFollowing: false});
+    };
+
+    startFollow = (event) => {
+        console.log(event.clientX, event.clientY)
+        this.setState({isFollowing: true});
+    };
+
+    removeMeuble = () => {
+        let newList = this.state.mesMeubles;
+        newList.splice(this.state.selectedMeuble,1);
+        this.setState({mesMeubles: this.state.mesMeubles})
+    }
+
+    updateMesMeubles = (e, property) => {
+        console.log(e)
+        this.setState(prevState => {
+            let newList = prevState.mesMeubles;
+            newList[this.state.selectedMeuble][property] = e;
+            console.log(newList[this.state.selectedMeuble])
+            return {mesMeubles: newList};
+        })
+    }
+
+    handleArrow = (e) => {
+        if(this.state.mesMeubles.length !== 0 && this.state.focus) {
+            console.log("okok")
+            // arrow up/down button should select next/previous list element
+            if (e.keyCode === 38) {
+                console.log('up')
+                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posY - 1, 'posY')
+            } else if (e.keyCode === 40) {
+                console.log('down')
+                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posY + 1, 'posY')
+            } else if (e.keyCode === 37) {
+                console.log('left')
+                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posX - 1, 'posX')
+            } else if (e.keyCode === 39) {
+                console.log('right')
+                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posX + 1, 'posX')
+            }
+        }
+    }
+
+    myPlaceRef: ?HTMLElement;
+
+    convertSizeWidth = (size) => {
+        // if (this.state.myPlace.long >= this.state.myPlace.larg) {
+            console.log(this.state.myPlace.long, size, size * 100 / this.state.myPlace.long)
+            return this.myPlaceRef ? (size * 100) / this.state.myPlace.long : 0;
+        // } else {
+        //     console.log(this.state.myPlace.larg, size, size * 100 / this.state.myPlace.larg)
+        //     return this.myPlaceRef ? (size * 100) / this.state.myPlace.larg : 0;
+        // }
+    }
+    convertSizeHeight = (size) => {
+        // if (this.state.myPlace.long >= this.state.myPlace.larg) {
+            console.log(this.state.myPlace.larg, size, size * 100 / this.state.myPlace.larg)
+            return this.myPlaceRef ? (size * 100) / this.state.myPlace.larg : 0;
+        // } else {
+        //     console.log(this.state.myPlace.long, size, size * 100 / this.state.myPlace.long)
+        //     return this.myPlaceRef ? (size * 100) / this.state.myPlace.long : 0;
+        // }
+    }
+
+    render() {
+        return (
+            <div className="home">
+                <div className="menu" onClick={() => this.setState({focus: false})}>
+                    <div className="tabs">
+                        <div
+                            className={`tab ${this.state.action !== 'CREATE' ? 'tab--not-selected' : ''}`}
+                            onClick={() => this.setState({action: 'CREATE'})}
+                        >
+                            Créer
+                        </div>
+                        <div
+                            className={`tab ${this.state.action !== 'MODIFY' ? 'tab--not-selected-right' : ''}`}
+                            onClick={() => this.setState({action: 'MODIFY'})}
+                        >
+                            Modifier
+                        </div>
+                    </div>
+                    {
+                        this.state.action === "CREATE" &&
+                        <Fragment>
+                            <div>
+                                <div className="label">longueur</div>
+                                <input
+                                    value={this.state.newMeuble.long}
+                                    type="number"
+                                    onChange={e => this.setState({newMeuble: { ...this.state.newMeuble, long: e.target.value }})}
+                                />
+                                <div className="label">largeur</div>
+                                <input
+                                    value={this.state.newMeuble.larg}
+                                    type="number"
+                                    onChange={e => this.setState({newMeuble: { ...this.state.newMeuble, larg: e.target.value }})}
+                                />
+                                <button className="button" onClick={this.addMeuble}>Créer meuble</button>
+                            </div>
+                        </Fragment>
+                    }
+                    {
+                        this.state.action === "MODIFY" &&
+                        <Fragment>
+                            <div className="label">longueur</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].long : ''}
+                                type="number"
+                                onChange={e => this.updateMesMeubles(e.target.value, "long")}
+                            />
+                            <div className="label">largeur</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].larg : ''}
+                                type="number"
+                                onChange={e => this.updateMesMeubles(e.target.value, "larg")}
+                            />
+                            <div className="label">color</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].color : ''}
+                                type="text"
+                                onChange={e => this.updateMesMeubles(e.target.value, "color")}
+                            />
+                            <div className="label">name</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].name : ''}
+                                type="string"
+                                onChange={e => this.updateMesMeubles(e.target.value, "name")}
+                            />
+                            <div className="label">posX</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].posX : ''}
+                                type="number"
+                                onChange={e => this.updateMesMeubles(e.target.value, "posX")}
+                            />
+                            <div className="label">posY</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].posY : ''}
+                                type="number"
+                                onChange={e => this.updateMesMeubles(e.target.value, "posY")}
+                            />
+                            <div className="label">angle</div>
+                            <input
+                                value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].angle : ''}
+                                type="number"
+                                onChange={e => this.updateMesMeubles(e.target.value, "angle")}
+                            />
+                            <button
+                                className="button"
+                                onClick={this.removeMeuble}
+                            >
+                                Enlever meuble
+                            </button>
+                        </Fragment>
+                    }
+                </div>
+                <div className="drawing-zone">
+                    {
+                        console.log(this.state.myPlace.long, this.state.myPlace.larg, this.state.myPlace.long >= this.state.myPlace.larg)
+                    }
+                    <div
+                        className="my-place"
+                        ref={e => this.myPlaceRef = e}
+                        style={{
+                            width: this.state.myPlace.long >= this.state.myPlace.larg ? '90%' : `${this.state.adaptedValue}px`,
+                            height: this.state.myPlace.long >= this.state.myPlace.larg ? `${this.state.adaptedValue}px` : '90%',
+                        }}
+                    >
+                        {
+                            this.state.mesMeubles.map((meuble, index) => (
+                                <div
+                                    key={Math.random()*100}
+                                    className="meuble"
+                                    onMouseDown={
+                                        (e) => this.setState({ selectedMeuble: index, newMeuble: {}, focus: true }, () => {
+                                        this.startFollow(e);
+                                    })}
+                                    // onMouseDown={this.startFollow}
+                                    onMouseUp={this.stopFollow}
+                                    style={{
+                                        width: `${this.convertSizeWidth(meuble.long)}%`,
+                                        height: `${this.convertSizeHeight(meuble.larg)}%`,
+                                        background: meuble.color || '',
+                                        top: `${meuble.posY}px` || '',
+                                        left: `${meuble.posX}px` || '',
+                                        transform: `rotate(${meuble.angle}deg)` || '',
+                                    }}
+                                >
+                                    {meuble.name}
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,253 +2,42 @@
 
 import React, { Fragment } from 'react';
 import type { Meuble } from '../types/Meubles';
+import DrawYourPlace from "./DrawYourPlace";
 
 type State = {
-    mesMeubles: Meuble[],
-    selectedMeuble: Number,
     myPlace: Meuble,
-    newMeuble: Meuble,
-    action: string,
-    isFollowing: boolean,
+    myPlaceIsCreated: boolean,
 }
 export default class Home extends React.Component<Props> {
     state = {
-        action: 'CREATE',
-        mesMeubles: [],
-        selectedMeuble: 0,
-        myPlace: {
-            long: 440,
-            larg: 505,
-        },
-        newMeuble: {posX: 0, posY: 0, angle: 0, color: '', name: ''},
-        isFollowing: false,
-    }
-    componentDidMount() {
-        console.log(this.state.myPlace.long * this.myPlaceRef.offsetHeight / this.state.myPlace.larg)
-        console.log(this.state.myPlace.long , this.myPlaceRef.offsetHeight , this.state.myPlace.larg)
-        if (this.state.myPlace.long >= this.state.myPlace.larg) {
-            console.log('long > larg')
-            this.setState({myPlace: { ...this.state.myPlace, larg: this.state.myPlace.larg * this.myPlaceRef.offsetWidth / this.state.myPlace.long } })
-        } else {
-            console.log('larg > long')
-            this.setState({myPlace: { ...this.state.myPlace, long: this.state.myPlace.long * this.myPlaceRef.offsetHeight / this.state.myPlace.larg } })
-        }
-
-        window.addEventListener('keydown',this.handleArrow)
+        myPlace: {long: 0, larg: 0},
+        myPlaceIsCreated: false,
     }
 
-    addMeuble = () => {
-        this.setState({
-            mesMeubles: [...this.state.mesMeubles, this.state.newMeuble],
-            newMeuble: {
-                posX: 0,
-                posY: 0,
-                larg: 0,
-                long: 0,
-            }
-        });
-    }
-
-    componentWillUpdate = (nextProps, nextState) => {
-        if(this.state.isFollowing !== nextState.isFollowing ) {
-            this.toggleFollowing(nextState.isFollowing);
-        }
-    };
-
-    toggleFollowing = (isEnable) => {
-        if (isEnable) {
-            window.addEventListener('mousemove', this.follow);
-            window.addEventListener('mouseup', this.stopFollow);
-        } else {
-            window.removeEventListener('mousemove', this.follow);
-        }
-    };
-
-    follow = (event) => {
-        this.updateMesMeubles(event.clientX - this.myPlaceRef.offsetLeft - this.state.mesMeubles[this.state.selectedMeuble].long/2, 'posX')
-        this.updateMesMeubles(event.clientY - this.myPlaceRef.offsetTop - this.state.mesMeubles[this.state.selectedMeuble].larg/2, 'posY')
-        console.log(event.clientX - this.myPlaceRef.offsetLeft, event.clientY - this.myPlaceRef.offsetTop)
-    };
-
-    stopFollow =  () => {
-        this.setState({isFollowing: false});
-    };
-
-    startFollow = (event) => {
-        console.log(event.clientX, event.clientY)
-        this.setState({isFollowing: true});
-    };
-
-    removeMeuble = () => {
-        let newList = this.state.mesMeubles;
-        newList.splice(this.state.selectedMeuble,1);
-        this.setState({mesMeubles: this.state.mesMeubles})
-    }
-
-    updateMesMeubles = (e, property) => {
-        console.log(e)
-        this.setState(prevState => {
-            let newList = prevState.mesMeubles;
-            newList[this.state.selectedMeuble][property] = e;
-            console.log(newList[this.state.selectedMeuble])
-            return {mesMeubles: newList};
-        })
-    }
-
-    handleArrow = (e) => {
-        if(this.state.mesMeubles.length !== 0) {
-            console.log("okok")
-            // arrow up/down button should select next/previous list element
-            if (e.keyCode === 38) {
-                console.log('up')
-                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posY - 1, 'posY')
-            } else if (e.keyCode === 40) {
-                console.log('down')
-                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posY + 1, 'posY')
-            } else if (e.keyCode === 37) {
-                console.log('left')
-                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posX - 1, 'posX')
-            } else if (e.keyCode === 39) {
-                console.log('right')
-                this.updateMesMeubles(this.state.mesMeubles[this.state.selectedMeuble].posX + 1, 'posX')
-            }
-        }
-    }
-
-    myPlaceRef: ?HTMLElement;
-
-    convertSize = (size) => {
-        console.log(this.myPlaceRef.offsetWidth, size, size * 100 / this.myPlaceRef.offsetWidth)
-        return this.myPlaceRef ? (size * 100) / this.myPlaceRef.offsetWidth : 0;
-    }
 
     render() {
+        if (!this.state.myPlaceIsCreated) {
+            return (
+                <div>
+                    <div className="label">longueur</div>
+                    <input
+                        value={this.state.myPlace.long}
+                        type="number"
+                        onChange={e => this.setState({myPlace: { ...this.state.myPlace, long: e.target.value }})}
+                    />
+                    <div className="label">largeur</div>
+                    <input
+                        value={this.state.myPlace.larg}
+                        type="number"
+                        onChange={e => this.setState({myPlace: { ...this.state.myPlace, larg: e.target.value }})}
+                    />
+                    <button className="button" onClick={() => this.setState({myPlaceIsCreated : true})}>Créer mon appart</button>
+                </div>
+            )
+        }
+
         return (
-            <div className="home">
-                <div className="menu">
-                    <div className="tabs">
-                        <div
-                            className={`tab ${this.state.action === 'CREATE' ? 'tab--selected' : ''}`}
-                            onClick={() => this.setState({action: 'CREATE'})}
-                        >
-                            Créer
-                        </div>
-                        <div
-                            className={`tab ${this.state.action === 'MODIFY' ? 'tab--selected' : ''}`}
-                            onClick={() => this.setState({action: 'MODIFY'})}
-                        >
-                            Modifier
-                        </div>
-                    </div>
-                    {
-                        this.state.action === "CREATE" &&
-                            <Fragment>
-                                <div>
-                                    <div>longueur</div>
-                                    <input
-                                        value={this.state.newMeuble.long}
-                                        type="number"
-                                        onChange={e => this.setState({newMeuble: { ...this.state.newMeuble, long: e.target.value }})}
-                                    />
-                                    <div>largeur</div>
-                                    <input
-                                        value={this.state.newMeuble.larg}
-                                        type="number"
-                                        onChange={e => this.setState({newMeuble: { ...this.state.newMeuble, larg: e.target.value }})}
-                                    />
-                                    <button onClick={this.addMeuble}>Créer meuble</button>
-                                </div>
-                            </Fragment>
-                    }
-                    {
-                        this.state.action === "MODIFY" &&
-                            <Fragment>
-                                <div>longueur</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].long : ''}
-                                    type="number"
-                                    onChange={e => { console.log(e.target.value) ; this.updateMesMeubles(e.target.value, "long")}}
-                                />
-                                <div>largeur</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].larg : ''}
-                                    type="number"
-                                    onChange={e => this.updateMesMeubles(e.target.value, "larg")}
-                                />
-                                <div>color</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].color : ''}
-                                    type="text"
-                                    onChange={e => this.updateMesMeubles(e.target.value, "color")}
-                                />
-                                <div>posX</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].posX : ''}
-                                    type="number"
-                                    onChange={e => this.updateMesMeubles(e.target.value, "posX")}
-                                />
-                                <div>name</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].name : ''}
-                                    type="string"
-                                    onChange={e => this.updateMesMeubles(e.target.value, "name")}
-                                />
-                                <div>posY</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].posY : ''}
-                                    type="number"
-                                    onChange={e => this.updateMesMeubles(e.target.value, "posY")}
-                                />
-                                <div>angle</div>
-                                <input
-                                    value={this.state.mesMeubles[this.state.selectedMeuble] ? this.state.mesMeubles[this.state.selectedMeuble].angle : ''}
-                                    type="number"
-                                    onChange={e => this.updateMesMeubles(e.target.value, "angle")}
-                                />
-                                <button
-                                    onClick={this.removeMeuble}
-                                >
-                                    Enlever meuble
-                                </button>
-                            </Fragment>
-                    }
-                </div>
-                <div className="drawing-zone">
-                    {
-                        console.log(this.state.myPlace.long, this.state.myPlace.larg, this.state.myPlace.long >= this.state.myPlace.larg)
-                    }
-                    <div
-                        className="my-place"
-                        ref={e => this.myPlaceRef = e}
-                        style={{
-                            width: this.state.myPlace.long >= this.state.myPlace.larg ? '90%' : `${this.state.myPlace.long}px`,
-                            height: this.state.myPlace.long >= this.state.myPlace.larg ? `${this.state.myPlace.larg}px` : '90%',
-                        }}
-                    >
-                        {
-                            this.state.mesMeubles.map((meuble, index) => (
-                                <div
-                                    key={Math.random()*100}
-                                    className="meuble"
-                                    onClick={() => this.setState({selectedMeuble: index})}
-                                    onMouseDown={this.startFollow}
-                                    onMouseUp={this.stopFollow}
-                                    style={{
-                                        width: `${this.convertSize(meuble.long)}%`,
-                                        height: `${this.convertSize(meuble.larg)}%`,
-                                        background: meuble.color || '',
-                                        top: `${meuble.posY}px` || '',
-                                        left: `${meuble.posX}px` || '',
-                                        transform: `rotate(${meuble.angle}deg)` || '',
-                                    }}
-                                >
-                                    {meuble.name}
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-            </div>
+            <DrawYourPlace myPlace={this.state.myPlace} />
         )
     }
 }
